644867287604d54c8423e42932ad1b64d5e34668
thegreatsantini <lss13@my.fsu.edu>
2018-04-19 16:15:54 -0700

    sprint 3 completed

--- models/album.js
+++ models/album.js
@@ -1,2 +1,15 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
+import { Model } from "mongoose";
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+    albumName: String,
+    name: String,
+    releasedData: String,
+    genres: [],
+})
+
+const Album = mongoose.model('Album', AlbumSchema);
+
+module.exports = Album;
